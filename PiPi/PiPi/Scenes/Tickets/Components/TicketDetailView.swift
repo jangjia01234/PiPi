5c82a33f0dd76c2c7e262ffb096ddc5d3a264949
Jia <71865277+jangjia01234@users.noreply.github.com>
2024-08-08 15:49:57 +0900

    Comment: 지도 코드 주석으로 임시 추가

--- PiPi/PiPi/Scenes/Tickets/Components/TicketDetailView.swift
+++ PiPi/PiPi/Scenes/Tickets/Components/TicketDetailView.swift
@@ -15,6 +15,10 @@ struct TicketDetailView: View {
     @Binding var isParticipantList: Bool
     @Binding var isLocationVisible: Bool
     
+    // TODO: 머지 후 주석 해제 (지도 코드 활용)
+    // @Namespace private var mapScope
+    // @State private var cameraPosition: MapCameraPosition = .defaultPosition
+    
     var activity: Activity
     var userProfile: UserProfile
     
@@ -36,11 +40,19 @@ struct TicketDetailView: View {
                             Text("참가자가 아직 없습니다.")
                         }
                     } else if isLocationVisible {
-                        Map() {
-                            // TODO: Level 조절 필요 (더 멀리)
-                            Marker("약속 장소", coordinate: CLLocationCoordinate2D(latitude: activity.coordinates.latitude, longitude: activity.coordinates.longitude))
-                                .tint(.accent)
-                        }
+                        // TODO: 머지 후 주석 해제 (지도 코드 활용)
+//                        Map(
+//                            position: $cameraPosition,
+//                            bounds: .init(
+//                                centerCoordinateBounds: .cameraBoundary,
+//                                minimumDistance: 500,
+//                                maximumDistance: 3000
+//                            ),
+//                            scope: mapScope
+//                        ) {
+//                            Marker("\(activity.title)", coordinate: CLLocationCoordinate2D(latitude: activity.coordinates.latitude, longitude: activity.coordinates.longitude))
+//                                .tint(.accent)
+//                        }
                     } else {
                         if activity.hostID == userProfile.id {
                             Text("주최자 닉네임: \(userProfile.nickname)")
