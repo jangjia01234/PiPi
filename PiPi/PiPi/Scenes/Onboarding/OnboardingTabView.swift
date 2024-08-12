6ddb7cffa5c7673e7f359866e37126e1a280bd17
Sangyun Jeong <jsangyun1039@naver.com>
2024-08-13 04:35:17 +0900

    Refactor: OnboardingTabView 리팩토링
    - Onboarding.swift 파일 삭제
    - OnboardingInfo 구현 및 적용

--- PiPi/PiPi/Scenes/Onboarding/OnboardingTabView.swift
+++ PiPi/PiPi/Scenes/Onboarding/OnboardingTabView.swift
@@ -10,40 +10,75 @@ import SwiftUI
 struct OnboardingTabView: View {
     
     init() {
-        // UIPageControl의 Appearance 설정
-        UIPageControl.appearance().currentPageIndicatorTintColor = .accent // 현재 페이지 색상
-        UIPageControl.appearance().pageIndicatorTintColor = UIColor.accent.withAlphaComponent(0.2) // 다른 페이지 색상
+        UIPageControl.appearance().currentPageIndicatorTintColor = .accent
+        UIPageControl.appearance().pageIndicatorTintColor = UIColor.accent.withAlphaComponent(0.2)
     }
     
-    @State private var navigateToProfile = false
-    @State private var isFirstLaunching = true
+    @State private var moveToProfile = false
     
     var body: some View {
-        NavigationStack{
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    moveToProfile = true
+                } label: {
+                    Text("건너뛰기")
+                        .foregroundColor(.accent)
+                        .font(.callout)
+                }
+            }
+            .padding()
+            
             TabView {
-                // 페이지 1: 포피와 애피
-                OnboardingPageView(imageName: "onboarding1",
-                                   title: "포피와 애피의 만남을 점으로 찍어보세요",
-                                   subtitle: "가깝l지만 먼 포스텍 피플과 애플 피플의 교집합", navigateToProfile: $navigateToProfile)
-                // 페이지 2: 활동 등록
-                OnboardingPageView(imageName: "onboarding2",
-                                   title: "우리가 같이 놀 수 있는 이벤트를 등록해요",
-                                   subtitle: "접점을 통해 우리가 만들어가는 플레이그라운드", navigateToProfile: $navigateToProfile)
-                // 페이지 3: 활동 참여
-                OnboardingPageView(imageName: "onboarding3",
-                                   title: "맘에 드는 이벤트를\n 신청해요",
-                                   subtitle: "다양한 카테고리로 모인 우리가 점을 찍어 만든 커넥팅 닷", navigateToProfile: $navigateToProfile)
-                // 페이지 4: QR 인증
-                OnboardingLastPageView(imageName: "onboarding4",
-                                       title: "QR로 활동을\n 인증해요",
-                                       subtitle: "직접 만나 활동을 인증하고 우리 사이 원 만들기", navigateToProfile: $navigateToProfile)
+                ForEach(onboardingInfo, id: \.title) { info in
+                    OnboardingPageView(imageName: info.imageName, title: info.title, subtitle: info.subtitle)
+                }
             }
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-            .fullScreenCover(isPresented: $navigateToProfile) {
+            .fullScreenCover(isPresented: $moveToProfile) {
                 OnboardingProfileView()
             }
         }
     }
+    
+}
+
+private extension OnboardingTabView {
+    
+    struct OnboardingInfo {
+        
+        let imageName: String
+        let title: String
+        let subtitle: String
+        
+    }
+    
+    var onboardingInfo: [OnboardingInfo] {
+        [
+            OnboardingInfo(
+                imageName: "onboarding1",
+                title: "포피와 애피의 만남을 점으로 찍어보세요",
+                subtitle: "가깝지만 먼 포스텍 피플과 애플 피플의 교집합"
+            ),
+            OnboardingInfo(
+                imageName: "onboarding2",
+                title: "우리가 같이 놀 수 있는 이벤트를 등록해요",
+                subtitle: "접점을 통해 우리가 만들어가는 플레이그라운드"
+            ),
+            OnboardingInfo(
+                imageName: "onboarding3",
+                title: "맘에 드는 이벤트를\n 신청해요",
+                subtitle: "다양한 카테고리로 모인 우리가 점을 찍어 만든 커넥팅 닷"
+            ),
+            OnboardingInfo(
+                imageName: "onboarding4",
+                title: "QR로 활동을\n 인증해요",
+                subtitle: "직접 만나 활동을 인증하고 우리 사이 원 만들기"
+            )
+        ]
+    }
+    
 }
 
 #Preview {
