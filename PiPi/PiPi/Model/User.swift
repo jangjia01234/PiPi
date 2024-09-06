33d644d3388e2e4f2eecb6d4e770a455a73f3f33
Sangyun Jeong <jsangyun1039@naver.com>
2024-09-07 00:42:08 +0900

    Refactor: User 데이터 모델 변경
    - User로 네이밍 수정
    - level 속성 삭제
    - Equatable 함수 명시 제거

--- PiPi/PiPi/Model/User.swift
+++ PiPi/PiPi/Model/User.swift
@@ -1,5 +1,5 @@
 //
-//  UserProfile.swift
+//  User.swift
 //  PiPi
 //
 //  Created by 신혜연 on 8/3/24.
@@ -7,39 +7,30 @@
 
 import Foundation
 
-struct UserProfile: Identifiable, Codable {
+struct User: Identifiable, Equatable, Codable {
     
     let id: String
     let nickname: String
     let affiliation: Affiliation
     let email: String
-    let level: Int
     
     init(
         id: String = UUID().uuidString,
         nickname: String,
         affiliation: Affiliation,
-        email: String,
-        level: Int = 1
+        email: String
     ) {
         self.id = id
         self.nickname = nickname
         self.affiliation = affiliation
         self.email = email
-        self.level = level
-    }
-    
-}
-
-extension UserProfile: Equatable {
-    
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.id == rhs.id
     }
     
 }
 
 enum Affiliation: String, Codable, CaseIterable {
+    
     case postech = "포항공대"
     case apple = "애플 디벨로퍼 아카데미"
+    
 }
