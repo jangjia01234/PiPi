20ef675f2521e08141a147a50cc33808f93da299
Sangyun Jeong <jsangyun1039@naver.com>
2024-09-07 20:17:38 +0900

    Feat: LocationAuthorizationView 구현

--- PiPi/PiPi/Scenes/Onboarding/OnboardingPageView.swift
+++ PiPi/PiPi/Scenes/Onboarding/OnboardingPageView.swift
@@ -14,26 +14,24 @@ struct OnboardingPageView: View {
     let subtitle: String
     
     var body: some View {
-        ZStack{
-            VStack {
-                Spacer()
-                Image(imageName)
-                    .resizable()
-                    .frame(width: 288, height: 290)
-                    .padding(.bottom, 36)
-                Text(title)
-                    .multilineTextAlignment(.center)
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.bottom, 26)
-                Text(subtitle)
-                    .multilineTextAlignment(.center)
-                    .font(.footnote)
-                    .foregroundColor(.gray)
-                Spacer()
-            }
-            .padding(.bottom, 20)
+        VStack {
+            Spacer()
+            Image(imageName)
+                .resizable()
+                .frame(width: 288, height: 290)
+                .padding(.bottom, 36)
+            Text(title)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 26)
+            Text(subtitle)
+                .multilineTextAlignment(.center)
+                .font(.footnote)
+                .foregroundColor(.gray)
+            Spacer()
         }
+        .padding(.bottom, 20)
     }
     
 }
